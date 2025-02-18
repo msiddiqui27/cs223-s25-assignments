e6383d075ab6b5f4de56e14bd9de6340b314f4cb
Maryam Siddiqui <msiddiqui@goldengate.cs.brynmawr.edu>
2025-02-18 16:51:21 -0500

    assignment complete

--- J02/strblock.c
+++ J02/strblock.c
@@ -1,3 +1,5 @@
+//Max and Maryam
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,22 +8,22 @@ int main()
 {
   char string[32];
   printf("Enter a string: ");
-  scanf("%s", string);
-
-  char** grid = malloc(32 * sizeof(char*));
+  scanf("%31s", string);
+  int len = strlen(string);
+  char** grid = malloc(len * sizeof(char*));
 
   for (int i = 0; i < strlen(string); i++) {
     grid[i] = malloc(32 * sizeof(char));
   }
 
-  grid[0] = string;
-  grid[strlen(string)-1] = string;
+  strcpy(grid[0], string);
+  strcpy(grid[strlen(string)-1], string);
   for( int i = 1; i < strlen(string)-1; i++){
     for( int j = 0; j < strlen(string); j++){
       if( i == j ){
         grid[i][i] = *(string+i);
       } else {
-        strcpy(&grid[i][j], " ");
+        grid[i][j] = ' ';
       }
     }
   }
