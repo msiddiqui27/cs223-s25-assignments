2fd85568dab424db08522986e6bbe9fe6dd77c17
Maryam Siddiqui <msiddiqui@goldengate.cs.brynmawr.edu>
2025-02-18 16:06:44 -0500

    code jam 2

--- J02/letter-list.c
+++ J02/letter-list.c
@@ -1,4 +1,83 @@
+//Max and Maryam
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct letter
+{
+  char letter;
+  struct letter *next;
+};
+
+struct letter *insertFirst(struct letter *head, char letter)
+{
+
+  struct letter *newLetter = malloc(sizeof(struct letter));
+
+
+  newLetter->letter = letter;
+
+  newLetter->next = head;
+
+  return newLetter;
+}
+
+void print(struct letter *head)
+{
+
+  struct letter *temp = head;
+
+
+  int index = 0;
+
+
+  while (temp)
+  {
+    printf("%c", temp->letter);
+    temp = temp->next;
+    index++;
+  }
+}
+
+void clear(struct letter *head)
+{
+
+  struct letter *next;
+
+
+  while (head)
+  {
+    next = head;
+    head = head->next;
+    free(next);
+  }
+}
+
 int main()
 {
+  char word[32];
+
+  char character;
+  printf("Enter a word: ");
+  scanf(" %s", word);
+
+  printf("Enter a char to remove: ");
+  scanf(" %c", &character);
+
+  struct letter *head = NULL;
+
+  for (int i = strlen(word); i >= 0; i--)
+  {
+    if (word[i] != character)
+    {
+      head = insertFirst(head, word[i]);
+    }
+  }
+
+  print(head);
+  printf("\n");
+
+  clear(head);
+
   return 0;
 }
--- J02/strblock.c
+++ J02/strblock.c
@@ -1,4 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int main()
 {
+  char string[32];
+  printf("Enter a string: ");
+  scanf("%s", string);
+
+  char** grid = malloc(32 * sizeof(char*));
+
+  for (int i = 0; i < strlen(string); i++) {
+    grid[i] = malloc(32 * sizeof(char));
+  }
+
+  grid[0] = string;
+  grid[strlen(string)-1] = string;
+  for( int i = 1; i < strlen(string)-1; i++){
+    for( int j = 0; j < strlen(string); j++){
+      if( i == j ){
+        grid[i][i] = *(string+i);
+      } else {
+        strcpy(&grid[i][j], " ");
+      }
+    }
+  }
+
+  for( int i = 0; i < strlen(string); i++){
+    for( int j = 0; j < strlen(string); j++){
+      printf("%c ", grid[i][j]);
+    }
+
+    printf("\n");
+  }
+
+  for(int i = 0; i< strlen(string); i++){
+    free(grid[i]);
+  }
+  free(grid);
+
   return 0;
 }
