f929acb05e7b0913a55ea817caa40318783120ae
Maryam Siddiqui <msiddiqui@goldengate.cs.brynmawr.edu>
2025-03-28 20:33:09 -0400

    assignment complete

--- A09/grep.c
+++ A09/grep.c
@@ -1,5 +1,161 @@
-#include <stdio.h>
+/**
+ * Author: Maryam
+ * Date: 03/28/2025
+ * Program description: grep.c implements a program that uses N processes to search for a keyword in a set of files
+ */
 
-int main() {
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_RESET "\x1b[0m"
+
+typedef struct Task
+{
+  int start_idx; // start index of file list this program will search
+  int end_idx;   // end index
+  char *keyword; // keyword being searched
+  int process_n; // process number for tracking
+  char **files;  // list of file names
+} Task;
+
+void fileSearch(Task k)
+/**
+ * performs the file search and exits w/ match count
+ */
+{
+  int match = 0;
+  printf("Process [%d] searching %d files ( %d to %d)\n", getpid(), k.end_idx - k.start_idx, k.start_idx, k.end_idx - 1);
+  // log which files are being searched by this process
+  fflush(stdout);
+
+  for (int i = k.start_idx; i < k.end_idx; i++) // loop through assigned file range
+  {
+    FILE *fp = fopen(k.files[i], "r"); // open file
+    if (!fp)
+    {
+      fprintf(stderr, "Process [%d] Erorr, cant open file %s\n", getpid(), k.files[i]);
+      fflush(stderr);
+      exit(0); // return/exit with 0 matches if file can't be opened
+    }
+    char line[1000]; // buffer to hold each line
+    while (fgets(line, sizeof(line), fp))
+    {
+      if (strstr(line, k.keyword))
+      {
+        printf(ANSI_COLOR_MAGENTA "%d] %s:%s" ANSI_COLOR_RESET, getpid(), k.files[i], line); // print match with color. was playing around with different colors and liked the magenta one the best.
+        fflush(stdout);
+        match++;
+      }
+    }
+    fclose(fp); // cloe file after reading
+  }
+
+  printf("Process [%d] found %d lines containing keyword: %s\n", getpid(), match, k.keyword);
+  fflush(stdout); // flush output before exiting
+
+  // return match count as exit code (maximum allowed is 255)
+  if (match > 255)
+  {
+    match = 225; // exit code limited from 0 to 255
+  }
+
+  exit(match); // send match count back by exiting code
+}
+
+void time_elapsed(struct timeval start, struct timeval end) 
+/**
+ * this function tracks the time elapsed
+ */
+{
+  double elapsed = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;//most of this was taken from chapter 11.5.1, which also taught me how to use the gettimeofday function. in the book they used 1.e6 and i just used 1000000 instead
+  printf("Elapsed time is %.6f\n", elapsed);
+}
+
+int main(int argc, char *argv[])
+/**
+ * main function for grep program
+ * implements grep command using multiple child processes. it divides a set of files aong n child processes to search for a given keyword and collects the total number of matches lines across all files
+ * @param argc: number of command line arguments
+ * @param argv: array of strings representing the command line arguments
+ * argv[1]: number of child processes to create
+ * argv[2]: keyword to search for
+ * argc[3]: list of file paths to search
+ * @return return 0 on successful exection, 1 for memory errors
+ */
+{
+  if (argc < 4)
+  {
+    fprintf(stderr, "usage: ./grep <NumProcesses> <Keyword> <Files>\n");//just copied the format from the assignment example
+    return 1;
+  }
+
+  int num_processes = atoi(argv[1]); // how many child processes to create
+  char *keyword = argv[2];           // keyword to search
+  char **files = &argv[3];           // file listes begins at argv[3]
+  int num_files = argc - 3;          // how many total files
+
+  printf("Searching %d files for keyword: %s\n", num_files, keyword);
+
+  struct timeval start, end;
+  gettimeofday(&start, NULL);            // record start time
+
+  int base = num_files / num_processes;  // base files for each process
+  int extra = num_files % num_processes; // files left over
+  int total_matches = 0;
+  int file_idx = 0;
+
+  pid_t *pids = malloc(sizeof(pid_t) * num_processes);
+  if (pids == NULL)
+  {
+    fprintf(stderr, "Error: malloc failed\n");
+    return 1;
+  }
+
+  for (int i = 0; i < num_processes; i++)
+  {
+    int count = base; // handle remainder
+    if (i < extra)
+    {
+      count += 1;
+    }
+
+    Task k;
+    k.start_idx = file_idx;//set starting index in files array for this process
+    k.end_idx = file_idx + count; //set ending index based on how many files this proess shuodl handle
+    k.keyword = keyword;//set keyword process will search
+    k.files = files;//assign shared array of file names to this task
+    k.process_n = i;//store this process's number so i knew where to debug if needed
+
+    pid_t pid = fork(); // fork child
+    if (pid == 0)
+    {
+      fileSearch(k); // child runs the search and exits with match count
+    }
+
+    pids[i] = pid;     // parent saves child’s pid
+    file_idx += count; // move to next file chunk
+  }
+
+  for (int i = 0; i < num_processes; i++)
+  {
+    int status;
+    waitpid(pids[i], &status, 0); // wait for specific child
+
+    
+    int matches = (status >> 8) & 0xFF;//extract child's exit code from the waitpid status, shifting tight by 8 bits to move the high byte int o the low positions since the actual exit code is in the hight byte. 
+                                      //& 0xFF masks it to keep just the lowest 8 bits. had to review this from chapter 4
+
+    total_matches = total_matches + matches; // add this child's match count to the running total
+  }
+
+  gettimeofday(&end, NULL); // record end time
+  printf("Total occurrences: %d\n", total_matches);
+  time_elapsed(start, end); // show total time
+  free(pids);               // clean up memory
   return 0;
 }
--- A09/warmup.c
+++ A09/warmup.c
@@ -1,5 +1,62 @@
+/**
+ * Author: Maryam
+ * Program description: warmup.c implemetns a program that spans processes according to the diagram on assignment 9.
+ * date: 03/27/2025
+ */
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-int main() {
+int main()
+/**
+ * main program following the sequence: A0-> B0-> C0-> Bye  (main)
+ *                                                |C1-> Bye (C0's child)
+ *                                           |B1-> Bye (B0's child)
+ * each process prints the messahe with its process id, using getpid()
+ * fflush(stdout) used to ensure immediate output before fork creates children, used form chapter 13
+ * waitpit() used from chapter 13 allowing parents to block while waitinf or the termination of a specific child process
+ */
+
+{
+  printf("%d] A0\n", getpid());
+  fflush(stdout);
+
+  printf("%d] B0\n", getpid());
+  fflush(stdout);
+
+  pid_t pid_b1 = fork(); // fork b1
+  if (pid_b1 == 0)       // child process: b1
+  {
+    printf("%d] B1\n", getpid());
+    fflush(stdout);
+    printf("%d] Bye\n", getpid());
+    fflush(stdout);
+    return 0;
+  }
+  waitpid(pid_b1, NULL, 0); // parent process continues as B0. waitpid() waits for B1 to finish
+
+  pid_t pid_c0 = fork(); // fork c0
+  if (pid_c0 == 0)       // child process: c0
+  {
+    printf("%d] C0\n", getpid());
+    fflush(stdout);
+    printf("%d] Bye\n", getpid()); // C0 says bye before C1
+    fflush(stdout);
+
+    pid_t pid_c1 = fork(); // fork c1
+    if (pid_c1 == 0)       // child process: c1
+    {
+      printf("%d] C1\n", getpid());
+      fflush(stdout);
+      printf("%d] Bye\n", getpid());
+      fflush(stdout);
+      return 0;
+    }
+
+    waitpid(pid_c1, NULL, 0); // C0 waits for C1
+    return 0;
+  }
+  waitpid(pid_c0, NULL, 0);
   return 0;
 }
