5c7783b6cdc337202c3203dca74c36977707ba97
Maryam Siddiqui <msiddiqui@goldengate.cs.brynmawr.edu>
2025-03-28 20:35:05 -0400

    assignment complete

--- A09/grep.c
+++ A09/grep.c
@@ -67,12 +67,12 @@ void fileSearch(Task k)
   exit(match); // send match count back by exiting code
 }
 
-void time_elapsed(struct timeval start, struct timeval end) 
+void time_elapsed(struct timeval start, struct timeval end)
 /**
  * this function tracks the time elapsed
  */
 {
-  double elapsed = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;//most of this was taken from chapter 11.5.1, which also taught me how to use the gettimeofday function. in the book they used 1.e6 and i just used 1000000 instead
+  double elapsed = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0; // most of this was taken from chapter 11.5.1, which also taught me how to use the gettimeofday function. in the book they used 1.e6 and i just used 1000000 instead
   printf("Elapsed time is %.6f\n", elapsed);
 }
 
@@ -90,7 +90,7 @@ int main(int argc, char *argv[])
 {
   if (argc < 4)
   {
-    fprintf(stderr, "usage: ./grep <NumProcesses> <Keyword> <Files>\n");//just copied the format from the assignment example
+    fprintf(stderr, "usage: ./grep <NumProcesses> <Keyword> <Files>\n"); // just copied the format from the assignment example
     return 1;
   }
 
@@ -102,7 +102,7 @@ int main(int argc, char *argv[])
   printf("Searching %d files for keyword: %s\n", num_files, keyword);
 
   struct timeval start, end;
-  gettimeofday(&start, NULL);            // record start time
+  gettimeofday(&start, NULL); // record start time
 
   int base = num_files / num_processes;  // base files for each process
   int extra = num_files % num_processes; // files left over
@@ -125,11 +125,11 @@ int main(int argc, char *argv[])
     }
 
     Task k;
-    k.start_idx = file_idx;//set starting index in files array for this process
-    k.end_idx = file_idx + count; //set ending index based on how many files this proess shuodl handle
-    k.keyword = keyword;//set keyword process will search
-    k.files = files;//assign shared array of file names to this task
-    k.process_n = i;//store this process's number so i knew where to debug if needed
+    k.start_idx = file_idx;       // set starting index in files array for this process
+    k.end_idx = file_idx + count; // set ending index based on how many files this proess shuodl handle
+    k.keyword = keyword;          // set keyword process will search
+    k.files = files;              // assign shared array of file names to this task
+    k.process_n = i;              // store this process's number so i knew where to debug if needed
 
     pid_t pid = fork(); // fork child
     if (pid == 0)
@@ -146,9 +146,8 @@ int main(int argc, char *argv[])
     int status;
     waitpid(pids[i], &status, 0); // wait for specific child
 
-    
-    int matches = (status >> 8) & 0xFF;//extract child's exit code from the waitpid status, shifting tight by 8 bits to move the high byte int o the low positions since the actual exit code is in the hight byte. 
-                                      //& 0xFF masks it to keep just the lowest 8 bits. had to review this from chapter 4
+    int matches = (status >> 8) & 0xFF; // extract child's exit code from the waitpid status, shifting tight by 8 bits to move the high byte int o the low positions since the actual exit code is in the hight byte.
+                                        //& 0xFF masks it to keep just the lowest 8 bits. had to review this from chapter 4
 
     total_matches = total_matches + matches; // add this child's match count to the running total
   }
