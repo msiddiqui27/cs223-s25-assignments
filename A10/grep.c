50e6235bf861aebc4dd533d443c3935795ef3a1c
Maryam Siddiqui <msiddiqui@goldengate.cs.brynmawr.edu>
2025-04-05 19:29:15 -0400

    assignment complete

--- A10/grep.c
+++ A10/grep.c
@@ -1,5 +1,110 @@
-#include <stdio.h>
 
-int main() {
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+
+typedef struct // struct holding arguments passed to each thread
+{
+  int thread_id;   // thread id
+  int start_index; // startinf idx of file range
+  int end_index;
+  char **files;
+  char *keyword;
+  int match_count; // number of matches thread found
+} ThreadArguments;
+
+void *search_files(void *arg)
+/**
+ * function to check if each thread runs
+ * searchws subset of files for keyword
+ * opens files to read, these files are assigned to a threat
+ * searches each line for the keywrod with strstr
+ * if there is a match, prints the line and thread/file info
+ * @param arg: pointer to threadArguments struct containign the arguments of the thread
+ * @return null
+ *
+ */
+{
+  ThreadArguments *args = (ThreadArguments *)arg;
+  args->match_count = 0; // initilaize match count for thread
+
+  for (int i = args->start_index; i < args->end_index; i++)
+  { // loop through file
+    FILE *fp = fopen(args->files[i], "r");
+    if (!fp) // open file and handle error case
+    {
+      fprintf(stderr, "thread can't open");
+    }
+    char line[1000];                      // holds each line
+    while (fgets(line, sizeof(line), fp)) // read each line from file
+    {
+      if (strstr(line, args->keyword)) // if line contains keywrod using strstr
+      {
+        printf("[thread %d] %s: %s", args->thread_id, args->files[i], line); // print match with file name and line
+        args->match_count++;                                                 // increment match counter
+      }
+    }
+    fclose(fp); // close file
+  }
+  return NULL;
+}
+int main(int argc, char *argv[])
+/**
+ * main function to run multithread grep
+ * parses command line arguments and splits the work among threads
+ */
+{
+  if (argc < 4)//check minimum number of arguments
+  {
+    fprintf(stderr, "./grep <NumThreards> <Keyword> <Files>\n");//print usage instructions like the example showed
+    exit(1);//exit
+  }
+
+  int num_threads = atoi(argv[1]);//convert argument 1 to number of threads
+  if (num_threads < 1)
+  {
+    fprintf(stderr, "Error: number of threads must be at least 1. try again.\n");//is number of threads positive?
+    exit(1);
+  }
+  char *keyword = argv[2];//keyword being searched
+  int num_files = argc - 3;//total number of files that are given to us
+  char **files = &argv[3];//point to first filename in argv
+
+  pthread_t *threads = malloc(num_threads * sizeof(pthread_t));//i tried another way but it didn't work so this way I am dynamically allocating arrays for threads and their arguemetns
+
+  ThreadArguments *thread_arguments = malloc(num_threads * sizeof(ThreadArguments));
+
+  if (threads == NULL || thread_arguments == NULL) //if memory allocation failed, print error message
+  {
+    fprintf(stderr, "error: memory allocation failed.\n");
+    exit(1);
+  }
+  int files_thread = num_files / num_threads;//calculate even diviison of files per thread
+  int remainder = num_files % num_threads;//leftover files
+
+  int current = 0;//next file to assign's index
+  for (int i = 0; i < num_threads; i++)
+  {
+    thread_arguments[i].thread_id = i;//assign thread id
+    thread_arguments[i].keyword = keyword;//keyword
+    thread_arguments[i].files = files;//pass file list
+    thread_arguments[i].start_index = current;//file range starting index
+  
+
+    current = thread_arguments[i].end_index;
+    pthread_create(&threads[i], NULL, search_files, &thread_arguments[i]);
+  }
+  int total_matches = 0;
+  for (int i = 0; i < num_threads; i++)
+  {
+    pthread_join(threads[i], NULL);
+    total_matches += thread_arguments[i].match_count;
+  }
+
+  printf("All threads done. Total matches: %d\n", total_matches);
+  free(threads);
+  free(thread_arguments);
   return 0;
 }
--- A10/marco_polo.c
+++ A10/marco_polo.c
@@ -1,4 +1,98 @@
-int main()
+/**
+ * Name: Maryam
+ * Date: 04/04/2025
+ * Assignment description: this programuses signals to coordinate between two processes, like so. When the user presses the 'm' key, the parents sends SIGALRM. When the child receives SIGALRM, the child prints "Marco" and sends SIGALRM to the parent. When the parent receives SIGALRM, the parent prints "Polo".
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h> //was getting an error using pid_t wihtout inlcuding this header file
+
+pid_t child_pid = -1; // global variables to hold child & parent process id
+pid_t parent_pid = -1;
+
+void parent_handler(int signum)
+/**
+ * function for the parent process when recieving sigalrm
+ * prints "polo" and the parent's pid when called
+ * @param signum: singla number recieved
+ */
 {
-  return 0;
+  printf("Polo [%d]\n", getpid()); // print polo and current process id
+  fflush(stdout);                  // flush output so it appears fast
+}
+
+void child_handler(int signum)
+/**
+ * signal handler for child process when recibing sigalrm
+ * prints marco and send sigalrm back to parent to compelte runnign
+ * @param signum: signal number received
+ */
+{
+  printf("Marco [%d]\n", getpid()); // print marco and current prcess id
+  fflush(stdout);                   // flush output out immediately
+  kill(parent_pid, SIGALRM);        // signal parent back with sigalrm
+}
+
+int main()
+/**
+ * main function running program
+ * forks into parent and child
+ * parents waits for m or q input
+ * q terminates
+ * return 0 when successfully exectued
+ */
+{
+  parent_pid = getpid(); // store parent process id
+  child_pid = fork();    // create child process
+
+  if (child_pid < 0) // if fork failed print message
+  {
+    printf("Error: the fork failed\n");
+    exit(1);
+  }
+
+  if (child_pid == 0) // runs in child process
+  {
+    signal(SIGALRM, child_handler);
+    signal(SIGTERM, exit);//tried to just exit bu the whole program crashed, so need to use sigterm instead for the signal process to exit
+
+    while (1) // this infininte loops wiats for a signal to happen
+    {
+      pause(); // also used in 13.4 of book from signals
+    }
+  }
+  else
+  {
+    signal(SIGALRM, parent_handler); // run parent process
+    printf("Parent is %d\n", parent_pid);
+    fflush(stdout);
+
+    char command; // user input
+    while (1)
+    {
+      command = getchar(); // read the variable from the user, which can only be m or q
+      if (command == '\n')
+      {
+        continue; // skip newlines
+      }
+
+      if (command == 'm')
+      {
+        kill(child_pid, SIGALRM); // send signal to child
+      }
+
+      else if (command == 'q')
+      {
+        kill(child_pid, SIGTERM); // terminate child
+        break;                    // exit loop
+      }
+      else
+      {
+        printf("incorrect input, try again\n");
+      }
+    }
+  }
 }
