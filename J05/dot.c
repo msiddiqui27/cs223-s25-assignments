b604bdef3b3a527ccfbf9be5e2587b411e06bcb8
Maryam Siddiqui <msiddiqui@goldengate.cs.brynmawr.edu>
2025-04-08 15:55:18 -0400

    assignment complete

--- J05/dot.c
+++ J05/dot.c
@@ -1,3 +1,4 @@
+// Gabby and Maryam
 #include <stdio.h>
 #include <time.h>
 #include <sys/time.h>
@@ -5,25 +6,87 @@
 #include <pthread.h>
 
 #define SIZE 100000
+#define thread_num 4
+int partial_sums[thread_num];
+int v[SIZE];
+int u[SIZE];
+
+typedef struct ThreadArguments
+{
+  int start_idx;
+  int end_idx;
+  int thread_id;
+} ThreadArguments;
 
 // your code here
+void *chunk_dotproduct(void *args)
+{
 
-int main(int argc, char *argv[]) {
+  ThreadArguments *threadargs = (ThreadArguments *)args;
+  int sum = 0;
+  for (int i = threadargs->start_idx; i < threadargs->end_idx; i++)
+  {
+    sum += u[i] * v[i];
+  }
+  partial_sums[threadargs->thread_id] = sum;
+
+  pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[])
+{
   srand(time(0));
-
-  int v[SIZE];
-  int u[SIZE];
   int dotproduct = 0;
-   
-  for (int i = 0; i < SIZE; i++) {
+
+  for (int i = 0; i < SIZE; i++)
+  {
     v[i] = rand() % 1000 - 500;
     u[i] = rand() % 1000 - 500;
     dotproduct += u[i] * v[i];
   }
   printf("Ground truth dot product: %d\n", dotproduct);
 
-  // TODO: Implement your thread solution here
+  pthread_t threads[thread_num];
+  ThreadArguments threadargs[thread_num];
+
+  int chunk_size = SIZE / thread_num;
+  // chunk 1
+  threadargs[0].thread_id = 0;
+  threadargs[0].start_idx = 0;
+  threadargs[0].end_idx = chunk_size;
+  // chunk 2
+  threadargs[1].thread_id = 1;
+  threadargs[1].start_idx = chunk_size;
+  threadargs[1].end_idx = chunk_size * 2;
+  // chunk 3
+  threadargs[2].thread_id = 2;
+  threadargs[2].start_idx = chunk_size * 2;
+  threadargs[2].end_idx = chunk_size * 3;
+  // chunk 4
+  threadargs[3].thread_id = 3;
+  threadargs[3].start_idx = chunk_size * 3;
+  threadargs[3].end_idx = SIZE;
+
+  pthread_create(&threads[0], NULL, chunk_dotproduct, &threadargs[0]);
+  pthread_create(&threads[1], NULL, chunk_dotproduct, &threadargs[1]);
+  pthread_create(&threads[2], NULL, chunk_dotproduct, &threadargs[2]);
+  pthread_create(&threads[3], NULL, chunk_dotproduct, &threadargs[3]);
+
+  // Wait for all 4 threads to finish
+  pthread_join(threads[0], NULL);
+  pthread_join(threads[1], NULL);
+  pthread_join(threads[2], NULL);
+  pthread_join(threads[3], NULL);
+
+  // Sum up the partial results
   int thread_dotproduct = 0;
+  thread_dotproduct += partial_sums[0];
+  thread_dotproduct += partial_sums[1];
+  thread_dotproduct += partial_sums[2];
+  thread_dotproduct += partial_sums[3];
+
+  // TODO: Implement your thread solution here
+
   printf("Test with 4 threads\n");
 
   printf("Answer with threads: %d\n", thread_dotproduct);
--- J05/multiply.c
+++ J05/multiply.c
@@ -1,3 +1,5 @@
+// Gabby and Maryam
+
 #include <stdio.h>
 #include <time.h>
 #include <sys/time.h>
@@ -5,37 +7,79 @@
 #include <pthread.h>
 
 #define SIZE 100
+#define NUM_THREADS 4
 
-// your code here
+int M[SIZE * SIZE];
+int u[SIZE];
+int result[SIZE];
+int result_threads[SIZE];
 
-int main(int argc, char *argv[]) {
-  srand(time(0));
+typedef struct
+{
+  int start_row;
+  int end_row;
+} ThreadData;
 
-  int M[SIZE*SIZE];
-  int u[SIZE];
-  int result[SIZE];
-  int result_threads[SIZE];
-   
-  for (int i = 0; i < SIZE; i++) {
-    u[i] = rand() % 10 - 5;
-    result[i] = 0;
+void *compute_product(void *arg)
+{
+  ThreadData *data = (ThreadData *)arg;
+
+  for (int i = data->start_row; i < data->end_row; i++)
+  {
+    result_threads[i] = 0;
+    for (int j = 0; j < SIZE; j++)
+    {
+      result_threads[i] += M[i * SIZE + j] * u[j];
+    }
   }
 
-  for (int i = 0; i < SIZE; i++) {
-    for (int j = 0; j < SIZE; j++) {
+  return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+  srand(time(0));
+
+  for (int i = 0; i < SIZE; i++)
+  {
+    u[i] = rand() % 10 - 5;
+    result[i] = 0;
+    result_threads[i] = 0;
+  }
+
+  for (int i = 0; i < SIZE; i++)
+  {
+    for (int j = 0; j < SIZE; j++)
+    {
       int value = rand() % 10 - 5;
-      M[i*SIZE + j] = value;
+      M[i * SIZE + j] = value;
       result[i] += value * u[j];
     }
   }
 
-  // TODO: Implement your thread solution here
   printf("Test with 4 threads\n");
 
+  pthread_t threads[NUM_THREADS];
+  ThreadData thread_data[NUM_THREADS];
 
+  int rows_per_thread = SIZE / NUM_THREADS;
+
+  for (int i = 0; i < NUM_THREADS; i++)
+  {
+    thread_data[i].start_row = i * rows_per_thread;
+    thread_data[i].end_row = (i == NUM_THREADS - 1) ? SIZE : (i + 1) * rows_per_thread;
+
+    pthread_create(&threads[i], NULL, compute_product, (void *)&thread_data[i]);
+  }
+
+  for (int i = 0; i < NUM_THREADS; i++)
+  {
+    pthread_join(threads[i], NULL);
+  }
 
   int error = 0;
-  for (int i = 0; i < SIZE; i++) {
+  for (int i = 0; i < SIZE; i++)
+  {
     error += abs(result[i] - result_threads[i]);
   }
   printf("Error between ground truth and multithreaded version: %d\n", error);
